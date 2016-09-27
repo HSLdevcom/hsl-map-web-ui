2b3b71f6a2f2a14130996389fec423474c7b8842
Sebastian <sebastian.hognabba@gmail.com>
2016-09-27 13:08:14 +0300

    Made header component responsive

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,10 @@ import styles from "./header.css";
 const Header = () => (
     <div className={styles.root}>
         <a href="/" className={styles.logo}>
-            <img src="https://www.hsl.fi/sites/all/themes/custom/hsl_tyyliopas/logo.png" alt="HSL / HRT"/>
+            <img
+              src="https://www.hsl.fi/sites/all/themes/custom/hsl_tyyliopas/logo.png"
+              alt="HSL / HRT"
+            />
         </a>
         <h1 className={styles.sectionTitle}>Kuljettajaohjeet</h1>
     </div>
