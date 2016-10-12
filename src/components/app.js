fb77d74b3cdabe7b3b17de89f06a628c5796e919
Sebastian <sebastian.hognabba@gmail.com>
2016-10-12 14:24:53 +0300

    Background color to cover whole screen

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Router, Route, browserHistory } from "react-router";
-
 import Header from "components/header";
 import Home from "components/home";
 import Map from "components/map";
+import style from "./app.css";
 
 const App = () => (
-    <div>
+    <div className={style.root}>
         <Header/>
         <Router history={browserHistory}>
             <Route path="/" component={Home}/>
--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,15 @@ import styles from "./header.css";
 
 const Header = () => (
     <div className={styles.root}>
-        <a href="/" className={styles.logo}>
-            <img
-              src="https://www.hsl.fi/sites/all/themes/custom/hsl_tyyliopas/logo.png"
-              alt="HSL / HRT"
-            />
-        </a>
-        <h1 className={styles.sectionTitle}>Kuljettajaohjeet</h1>
+        <div>
+            <a href="/" className={styles.logo}>
+                <img
+                  src="https://www.hsl.fi/sites/all/themes/custom/hsl_tyyliopas/logo.png"
+                  alt="HSL / HRT"
+                />
+            </a>
+            <h1 className={styles.sectionTitle}>Kuljettajaohjeet</h1>
+        </div>
     </div>
 );
 
