6560b701f839ab265b6046bab9b685fefe69fed2
Sebastian <sebastian.hognabba@gmail.com>
2016-10-25 14:05:30 +0300

    Split server and devserver to different files, added start scripts

---
+++ server-dev.js
@@ -0,0 +1,16 @@
+const webpack = require("webpack");
+const WebpackDevServer = require("webpack-dev-server");
+const config = require("./webpack.config");
+
+new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    stats: {
+        colors: true,
+    },
+})
+.listen(3000, "0.0.0.0", (err) => {
+    if (err) console.log(err);
+    console.log("Listening at port 3000");
+});
--- server.js
+++ server.js
@@ -1,35 +1,18 @@
 const express = require("express");
-const webpack = require("webpack");
-const WebpackDevServer = require("webpack-dev-server");
 const config = require("./webpack.config");
 
 const staticPath = config.output.path;
-const environment = process.env.NODE_ENV || "development";
 const app = express();
 
-if (environment === "production") {
-    app.use(express.static(staticPath))
-    .get("/*", (req, res) => {
-        res.sendFile("index.html", {
-            root: staticPath,
-        });
-    })
-    .listen(3000, "0.0.0.0", (err) => {
-        if (err) console.log(err);
-        console.log("Listening at port 3000");
+app
+.use(express.static(staticPath))
+.get("/*", (req, res) => {
+    res.sendFile("index.html", {
+        root: staticPath,
     });
-} else {
-    new WebpackDevServer(webpack(config), {
-        publicPath: config.output.publicPath,
-        hot: true,
-        historyApiFallback: true,
-        stats: {
-            colors: true,
-        },
-    })
-    .listen(3000, "0.0.0.0", (err) => {
-        if (err) console.log(err);
-        console.log("Listening at port 3000");
-    });
-}
+})
+.listen(3000, "0.0.0.0", (err) => {
+    if (err) console.log(err);
+    console.log("Listening at port 3000");
+});
 
