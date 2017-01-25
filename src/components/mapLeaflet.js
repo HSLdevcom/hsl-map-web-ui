f27da76f8aa2bdae4c9af96a221027838972da4d
Sebastian <sebastian.hognabba@gmail.com>
2017-01-25 16:37:09 +0200

    Changed use of timing stop according to change in hsl-map-generator-server

--- src/components/mapLeaflet.js
+++ src/components/mapLeaflet.js
@@ -30,10 +30,10 @@ const addMarkersToLayer = (stops, direction, map) => {
         timeIcon = timeIcon2;
     }
     stops.forEach((stop, index) => {
-        console.log(stop.timingStopType);
         let setIcon;
         if (index === 0) setIcon = routeIcon(startIcon);
-        else if (stop.isTiming) setIcon = routeIcon(timeIcon);
+        // TODO different icon for different type of timing stop (timingStopType === 1 or 2)
+        else if (stop.timingStopType > 0) setIcon = routeIcon(timeIcon);
         else setIcon = stopIcon(styles.stopIcon, directionStyle);
         L.marker(
             [stop.lat, stop.lon],
