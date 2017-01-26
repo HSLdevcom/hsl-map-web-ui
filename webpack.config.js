0536479a68277ddd845101e6f243d86fcaa1a36f
Sebastian <sebastian.hognabba@gmail.com>
2017-01-26 13:23:19 +0200

    Use relative path for api url

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ function getPlugins(env) {
             new webpack.DefinePlugin({
                 "process.env": {
                     NODE_ENV: JSON.stringify("production"),
-                    API_URL: JSON.stringify("http://136.243.66.163"),
+                    API_URL: JSON.stringify(""),
                 },
             }),
             new webpack.optimize.UglifyJsPlugin({ compressor: { warnings: false } }),
