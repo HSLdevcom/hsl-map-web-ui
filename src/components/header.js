2ebeb42d98b85d91e848dbad544171994f47b3aa
Sebastian <sebastian.hognabba@gmail.com>
2017-02-28 11:09:00 +0200

    Updated component header

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Link } from "react-router";
 import styles from "./header.css";
+import hslLogo from "../icons/hsl-logo.png";
 
 const Header = () => (
     <div className={styles.root}>
         <Link to={{ pathname: "/kuljettaja" }}>
             <div className={styles.logo}>
                 <img
-                  src="https://www.hsl.fi/sites/all/themes/custom/hsl_tyyliopas/logo.png"
+                  src={hslLogo}
                   alt="HSL / HRT"
                 />
             </div>
         </Link>
-        <h1 className={styles.sectionTitle}>Kuljettajaohjeet</h1>
+        <h2 className={styles.sectionTitle}>Kuljettajaohjeet</h2>
     </div>
 );
 
