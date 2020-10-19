58cb2355f491627bea0cf4a9974ef318c8d2890d
Petteri Pesonen <teemu.p.pesonen@gmail.com>
2020-10-19 13:22:29 +0300

    Update travis build script

--- travis-build.sh
+++ travis-build.sh
@@ -3,6 +3,7 @@ set -e
 
 ORG=${ORG:-hsldevcom}
 DOCKER_TAG=${TRAVIS_BUILD_NUMBER:-latest}
+BUILD_AND_PUSH_BRANCHES=('development', 'stage', 'master')
 
 if [[ $TRAVIS_BRANCH == "development" ]]; then
   DOCKER_TAG=dev
@@ -17,13 +18,20 @@ if [[ $TRAVIS_BRANCH == "master" ]]; then
 fi
 
 DOCKER_IMAGE=$ORG/hsl-map-web-ui:${DOCKER_TAG}
-DOCKER_IMAGE_LATEST=$ORG/hsl-map-web-ui:latest
+
+echo "Building image with tag -> ${DOCKER_TAG}"
 
 docker build --tag=$DOCKER_IMAGE .
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-  docker login -u $DOCKER_USER -p $DOCKER_AUTH
-  docker push $DOCKER_IMAGE
-  docker tag $DOCKER_IMAGE $DOCKER_IMAGE_LATEST
-  docker push $DOCKER_IMAGE_LATEST
+  if [[ " ${BUILD_AND_PUSH_BRANCHES[*]} " == *"$DOCKER_TAG"* ]]; then
+    echo "Pushing builded image to registry with tag -> ${DOCKER_TAG}"
+
+    docker login -u $DOCKER_USER -p $DOCKER_AUTH
+    docker push $DOCKER_IMAGE
+  else
+    echo "Pushed branch is not targeted environment branch (development, stage, production). Image is not pushed to registry"
+  fi
+else
+  echo "Image is not pushed to registry for pull requests"
 fi
