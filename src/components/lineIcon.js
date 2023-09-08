5a6443754fb798bb575d273f5a0f7371b8d67da4
Juho Hänninen <juho.hanninen@cgi.com>
2023-09-08 15:44:09 +0300

    Add icons and css styles for pikaratikka
    Also remove some unused styles and use colors as variables

--- src/components/lineIcon.js
+++ src/components/lineIcon.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classNames from "classnames";
 import BusIcon from "../icons/icon-bus-station.js";
+import LRailIcon from "../icons/icon-lrail.js";
 import TramIcon from "../icons/icon-tram.js";
 import TrunkIcon from "../icons/icon-trunk-station.js";
 import styles from "./lineIcon.module.css";
@@ -19,13 +20,16 @@ const LineIcon = ({
         <TrunkIcon height={iconSize} />
       ) : transportType === "TRAM" ? (
         <TramIcon height={iconSize} />
+      ) : transportType === "L_RAIL" ? (
+        <LRailIcon height={iconSize} />
       ) : (
         <BusIcon height={iconSize} />
       )}
       <span
         className={classNames(styles.lineNumber, {
+          [styles.lrail]: transportType === "L_RAIL",
           [styles.tram]: transportType === "TRAM",
-          [styles.bus]: transportType !== "TRAM",
+          [styles.bus]: transportType === "BUS",
           [styles.trunk]: trunkRoute,
         })}>
         {shortName}
